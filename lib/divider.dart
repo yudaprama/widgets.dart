dd24ab8d14bf21821679c309d35ca7210ac94f0e
[TD-Engineering] Yuda Prama <yudhaprama@icloud.com>
2020-05-04 03:20:39 +0700

    divider, space between

---
+++ lib/divider.dart
@@ -0,0 +1,15 @@
+library widgets;
+
+import 'package:flutter/material.dart';
+
+const dividerIndent = const Divider(height: 0.0, indent: 16.0,);
+const divider0 = const Divider(height: 0.0);
+
+const sizeBox25 = const SizedBox(height: 25);
+const sizeBox15 = const SizedBox(height: 15);
+const sizeBox10 = const SizedBox(height: 10);
+const sizeBox5 = const SizedBox(height: 5);
+
+const widthBox5 = const SizedBox(width: 5);
+const widthBox10 = const SizedBox(width: 10);
+const widthBox15 = const SizedBox(width: 15);
---
+++ lib/space_between.dart
@@ -0,0 +1,17 @@
+library widgets;
+
+import 'package:flutter/material.dart';
+
+Widget spaceBetween(List<Widget> children) {
+	return Row(
+		mainAxisAlignment: MainAxisAlignment.spaceBetween,
+		children: children,
+	);
+}
+
+Widget spaceBetweenPadding({EdgeInsets padding, List<Widget> children}) {
+	return Padding(
+		padding: padding,
+		child: spaceBetween(children),
+	);
+}
